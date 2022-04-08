898fb4269c7fda6fe923733bed9dddc86c520d5b
ZubayerAhammedTolon <zs.tolon0073@gmail.com>
2022-04-08 21:05:12 +0600

    router setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { Route, Router } from 'react-router';
 import './App.css';
+import Home from './components/Home/Home';
+import Volunteers from './components/Volunteers/Volunteers';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/volunteers' element={<Volunteers></Volunteers>}></Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    const number = 56;
+    return (
+        <div>
+            <h4>my number: {number}</h4>
+            <button>Click Me</button>
+
+        </div>
+    );
+};
+
+export default Home;
